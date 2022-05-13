d2b8614112c5b27a82fecd80fd29fdf15f237895
izumichiDana <kurbanbekovadana@gmail.com>
2022-05-13 19:06:02 +0800

    str

--- userprofile/models.py
+++ userprofile/models.py
@@ -5,8 +5,8 @@ from account.models import MyUser
 class Profile(models.Model):
     user = models.OneToOneField(MyUser, on_delete=models.CASCADE, related_name='user_profile')
 
-    avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
+    avatar = models.ImageField(upload_to='posts', blank=True, null=True)
     bio = models.TextField()
 
     def __str__(self):
-        return self.user
+        return self.user.email
