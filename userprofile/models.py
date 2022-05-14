6cdc61e75135daa361b569d9baadf417e5489adb
Nasihatiko <knasyikat@gmail.com>
2022-05-14 22:06:24 +0600

    pull

---
+++ userprofile/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from account.models import MyUser
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(MyUser, on_delete=models.CASCADE, related_name='user_profile')
+
+    avatar = models.ImageField(upload_to='posts', blank=True, null=True)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.user.email
