31d0692b3d8d91add46325c03fa41d6245fb2e1c
izumichiDana <kurbanbekovadana@gmail.com>
2022-05-16 12:52:08 +0800

    activation code

---
+++ account/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+
+from .models import MyUser
+from .utils import *
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(min_length=6, write_only=True)
+    password_confirm = serializers.CharField(min_length=6, write_only=True)
+    
+    def validate(self, validated_data):
+        password = validated_data.get('password')
+        password_confirm = validated_data.get('password_confirm')
+        if password != password_confirm:
+            raise serializers.ValidationError('Paasword do not mach')
+        return validated_data
+
+
+    class Meta:
+        model = MyUser
+        fields = ('email', 'password', 'password_confirm')
+
+    def create(self, validated_data):
+        email = validated_data.get('email')
+        password = validated_data.get('password')
+        user = MyUser.objects.create_user(email=email, password=password)
+        send_activation_code(email=user.email, activation_code=user.activation_code)
+        return user
+
+
+
+#TODO: login serializer
--- account/views.py
+++ account/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from rest_framework.views import APIView
+from .serializers import *
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+class RegisterView(APIView):
+    def post(self, request):
+        data = request.data
+        serializer = RegisterSerializer(data=data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response("Successfuly signed up!", status=status.HTTP_201_CREATED)
+
+
+
+class ActivateView(APIView):
+    def get(self, request, activation_code):
+        User = get_user_model()
+        user = get_object_or_404(User, activation_code=activation_code)
+        user.is_active = True
+        user.activation_code = ''
+        user.save()
+        return Response('Your account saccessfuly activated!', status=status.HTTP_200_OK)
+
+
+
+#TODO: login view 
+#TODO: logout view 
