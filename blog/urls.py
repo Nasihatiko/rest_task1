344b3d2d26e8ee73f1a043fc213751e09ef80882
Nasihatiko <knasyikat@gmail.com>
2022-05-16 10:12:01 +0600

    post

--- blog/urls.py
+++ blog/urls.py
@@ -14,10 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
 
+from post.views import CategoryListView, PostsViewSet, PostImageView
+
+router = DefaultRouter()
+router.register('posts', PostsViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('v1/api/categories/', CategoryListView.as_view()),
+    path('v1/api/add-image/', PostImageView.as_view()),
+    path('v1/api/', include(router.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ post/urls.py
@@ -0,0 +1,10 @@
+# from django.urls import path
+# from post import views
+#
+# urlpatterns = [
+#     path('categories/', views.CategoryListView.as_view(), name='categories-list'),
+#     path('posts/', views.PostView.as_view(), name='post-lists'),
+#     path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+#     path('posts-update/<int:pk>/', views.PostUpdateView.as_view()),
+#     path('posts-delete/<int:pk>/', views.PostDeleteView.as_view()),
+# ]
